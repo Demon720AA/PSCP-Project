af30be05ecd69b7560fc3a48ced08be8192a10c2
Supanut Arun <139570148+Demon720AA@users.noreply.github.com>
2023-09-23 13:10:29 +0700

    Coment setup2.py
    Coment liine18, 27

--- Space Invader EDIT/setup2.py
+++ Space Invader EDIT/setup2.py
@@ -15,7 +15,7 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 
 # load image
-background = pygame.image.load('graphics/background.jpg')
+background = pygame.image.load('graphics/background.jpg')    # Background
 
 def draw_bg():
     screen.blit(background, (0, 0))
@@ -24,7 +24,7 @@ def draw_bg():
 class Spaceship(pygame.sprite.Sprite):
     def __init__(self, x, y, health):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('')
+        self.image = pygame.image.load('')    # play image
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
         self.health_start = health
